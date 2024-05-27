ae661eee0844193705dc4f3df3370181b515e2b3
waltertaya <waltermittybecker5@gmail.com>
2024-05-27 19:52:58 +0300

    Added: 8-getBudgetCurrentYear

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,14 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
+
+  return {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
+  };
+}
