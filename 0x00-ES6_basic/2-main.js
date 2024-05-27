d94c3d51e69404079525e57bf968469e6a328c5f
waltertaya <waltermittybecker5@gmail.com>
2024-05-27 18:52:04 +0300

    Added: 2-arrow.js

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from './2-arrow';
+
+const neighborhoodsList = new getNeighborhoodsList(); //eslint-disable-line
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
