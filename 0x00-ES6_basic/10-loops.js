10b2c555e9120cc3e610ec2c094e79f8767a2f19
waltertaya <waltermittybecker5@gmail.com>
2024-05-27 20:36:43 +0300

    Updated: 10-loops, using .map

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [index, value] of array.entries()) {
-    array[index] = appendString + value; // eslint-disable-line
-  }
-
-  return array;
+  return array.map((value) => appendString + value);
 }
