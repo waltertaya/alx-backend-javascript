114aa0098c92adcd4a037261bf72e3e08b63a7d5
waltertaya <waltermittybecker5@gmail.com>
2024-05-28 23:06:10 +0300

    Updated: Task3, 3-all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
+    .catch(new Error('Signup system offline'));
 }
