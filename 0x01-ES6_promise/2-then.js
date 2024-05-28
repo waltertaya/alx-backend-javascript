cd8c0148f11bf4672304c4e30fb391225262b4c7
waltertaya <waltermittybecker5@gmail.com>
2024-05-28 22:58:37 +0300

    Updated: Task2, 2-then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => ({ status: 200, body: 'Success' }))
-    .then(() => console.log('Got a response from the API'))
-    .catch(() => new Error(''));
+    .catch(() => new Error(''))
+    .finally(() => console.log('Got a response from the API'));
 }
