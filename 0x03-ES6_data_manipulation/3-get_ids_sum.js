738d1aac386ed26ebcf1e9293e7b6becaffe1f63
waltertaya <waltermittybecker5@gmail.com>
2024-06-04 18:03:03 +0300

    Added: Task4

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,8 @@
 export default function getStudentIdsSum(students) {
+  if (!(Array.isArray(students))) {
+    return 0;
+  }
   const sum = students.reduce((total, num) => total + num.id);
+  console.log(sum);
   return sum;
 }
