25a6c4c776d31579899770b2fefeb7f5fcaf96d8
waltertaya <waltermittybecker5@gmail.com>
2024-07-24 20:56:26 +0300

    Updated: Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,9 @@
 const readline = require('readline');
 
-const rl = readline.createInterface(process.stdin, process.stdout);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 console.log('Welcome to Holberton School, what is your name?');
 
