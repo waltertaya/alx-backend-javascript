9f0cee5480a179f8f4ffbb2d3adc1191d2e8b4d6
waltertaya <waltermittybecker5@gmail.com>
2024-07-25 19:10:08 +0300

    Updated: Task 5::http server (correct output)

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,29 +4,65 @@ const fs = require('fs');
 const PORT = 1245;
 const HOSTNAME = 'localhost';
 
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (data) {
+      const fileLines = data
+        .toString('utf-8')
+        .trim()
+        .split('\n');
+      const studentGroups = {};
+      const dbFieldNames = fileLines[0].split(',');
+      const studentPropNames = dbFieldNames
+        .slice(0, dbFieldNames.length - 1);
+
+      for (const line of fileLines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord
+          .slice(0, studentRecord.length - 1);
+        const field = studentRecord[studentRecord.length - 1];
+        if (!Object.keys(studentGroups).includes(field)) {
+          studentGroups[field] = [];
+        }
+        const studentEntries = studentPropNames
+          .map((propName, idx) => [propName, studentPropValues[idx]]);
+        studentGroups[field].push(Object.fromEntries(studentEntries));
+      }
+
+      const totalStudents = Object
+        .values(studentGroups)
+        .reduce((pre, cur) => (pre || []).length + cur.length);
+      console.log(`Number of students: ${totalStudents}`);
+      for (const [field, group] of Object.entries(studentGroups)) {
+        const studentNames = group.map((student) => student.firstname).join(', ');
+        console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+      }
+      resolve(true);
+    }
+  });
+});
+
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     res.write('Hello Holberton School!');
     res.end();
-  } else if (req.url === '/students') {
-    const database = process.argv[2];
-    fs.readFile(database, 'utf8', (err, data) => {
-      if (err) {
-        res.write('This is the list of our students\n');
-        res.write('Cannot load the database\n');
+  }
+  if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2])
+      .then(() => {
         res.end();
-      } else {
-        const lines = data.split('\n').filter((line) => line !== '');
-        const students = lines.map((line) => line.split(','));
-        const csStudents = students.filter((student) => student[3] === 'CS');
-        const sweStudents = students.filter((student) => student[3] === 'SWE');
-        res.write('This is the list of our students\n');
-        res.write(`Number of students: ${students.length}\n`);
-        res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.map((student) => student[0]).join(', ')}\n`);
-        res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map((student) => student[0]).join(', ')}`);
-        res.end();
-      }
-    });
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
   }
 });
 
